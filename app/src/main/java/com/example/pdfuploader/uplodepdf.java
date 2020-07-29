a4ad628a9e3a1b53233d07da04178282e7a611e7
BHUPESH <bhoopeshmaurya147369@gmail.com>
2020-07-29 12:07:34 +0530

    PDF_Reader

---
+++ app/src/main/java/com/example/pdfuploader/uplodepdf.java
@@ -0,0 +1,25 @@
+package com.example.pdfuploader;
+
+public class uplodepdf {
+
+    public String name;
+    public String url;
+
+    public uplodepdf() {
+    }
+
+    public uplodepdf(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+}
